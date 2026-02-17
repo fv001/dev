af389ba32a45315bf0607dcd7d0ec6353df7eb6d
fv001 <fv8956@srmist.edu.in>
2026-02-17 10:12:31 +0530

    Print OOPS banner using String array and loop

---
+++ UC4.java
@@ -0,0 +1,20 @@
+public class UC4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            " *****   *****   *****   ***** ",
+            "*     * *     * *     * *      ",
+            "*     * *     * *     * *      ",
+            "*     * *     * *****   *****  ",
+            "*     * *     * *            * ",
+            "*     * *     * *            * ",
+            " *****   *****  *       ****** "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
+
