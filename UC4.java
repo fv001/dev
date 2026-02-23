d2e3dc901935376560b6d0cd4badc4d4f0f86532
fv001 <fv8956@srmist.edu.in>
2026-02-23 08:55:03 +0530

    changed the code as per instruction
    changed the code as per instruction

--- UC4.java
+++ UC4.java
@@ -2,19 +2,18 @@ public class UC4 {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            " *****   *****   *****   ***** ",
-            "*     * *     * *     * *      ",
-            "*     * *     * *     * *      ",
-            "*     * *     * *****   *****  ",
-            "*     * *     * *            * ",
-            "*     * *     * *            * ",
-            " *****   *****  *       ****** "
-        };
+        String[] bannerLines = new String[7];
 
-        for (String line : banner) {
+        bannerLines[0] = String.join("  ", " *** ", " *** ", "**** ", " *** ");
+        bannerLines[1] = String.join("  ", "*   *", "*   *", "*   *", "*    ");
+        bannerLines[2] = String.join("  ", "*   *", "*   *", "*   *", "*    ");
+        bannerLines[3] = String.join("  ", "*   *", "*   *", "**** ", " *** ");
+        bannerLines[4] = String.join("  ", "*   *", "*   *", "*    ", "    *");
+        bannerLines[5] = String.join("  ", "*   *", "*   *", "*    ", "    *");
+        bannerLines[6] = String.join("  ", " *** ", " *** ", "*    ", " *** ");
+
+        for (String line : bannerLines) {
             System.out.println(line);
         }
     }
 }
-
